4cab2f76c0d49f6c96da73bca6092078442baf9c
josei <koga1.jyo.73@gmail.com>
2024-06-04 01:39:21 +0900

    FizzBuzz問題を書いた

---
+++ fizzbuzz.rb
@@ -0,0 +1,16 @@
+def fizzbuzz
+    (1..20).each{|n|
+        if n % 3 == 0 && n % 5 == 0
+            puts "fizzbuzz"
+        elsif n % 3 == 0
+            puts "fizz"
+        elsif n % 5 == 0
+            puts "buzz"
+        else
+            puts n
+        end
+        
+    }
+end
+
+fizzbuzz
