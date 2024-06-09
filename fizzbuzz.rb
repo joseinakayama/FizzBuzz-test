68110f45d9567e8e1ade3861821d050846189654
josei <koga1.jyo.73@gmail.com>
2024-06-09 23:56:37 +0900

    インデントのspaceからtabに変更

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,16 +1,15 @@
 def fizzbuzz
-    (1..20).each{|n|
-        if n % 3 == 0 && n % 5 == 0
-            puts "fizzbuzz"
-        elsif n % 3 == 0
-            puts "fizz"
-        elsif n % 5 == 0
-            puts "buzz"
-        else
-            puts n
-        end
-        
-    }
+	(1..20).each{|n|
+		if n % 3 == 0 && n % 5 == 0
+			puts "fizzbuzz"
+		elsif n % 3 == 0
+			puts "fizz"
+		elsif n % 5 == 0
+			puts "buzz"
+		else
+			puts n
+		end
+	}
 end
 
 fizzbuzz
